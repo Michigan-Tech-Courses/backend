ddae8ce03d440b2f0e3118a31bd29dc2459d9635
Max Isom <hi@maxisom.me>
2021-01-24 17:59:49 -0500

    Use Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
 import {NestFactory} from '@nestjs/core';
 import {ValidationPipe} from '@nestjs/common';
+import {
+	FastifyAdapter,
+	NestFastifyApplication
+} from '@nestjs/platform-fastify';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
 	app.useGlobalPipes(new ValidationPipe({
 		forbidNonWhitelisted: true,
 		whitelist: true
 	}));
 
-	await app.listen(3000);
+	await app.listen(3000, '0.0.0.0');
 }
 
 void bootstrap();
