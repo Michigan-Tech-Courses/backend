fc3cb3fcb55b091af1227d84344839f9a78bd182
Max Isom <hi@maxisom.me>
2021-01-24 18:06:46 -0500

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {
 	FastifyAdapter,
 	NestFastifyApplication
 } from '@nestjs/platform-fastify';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,14 @@ async function bootstrap() {
 		forbidNonWhitelisted: true,
 		whitelist: true
 	}));
+
+	const config = new DocumentBuilder()
+		.setTitle('Michigan Tech Courses')
+		.setDescription('An API for courses and other information.')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
 
 	await app.listen(3000, '0.0.0.0');
 }
