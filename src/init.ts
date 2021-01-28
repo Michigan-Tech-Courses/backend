971584c3e1c78151a05e56a95ea9d338797a8a19
Max Isom <hi@maxisom.me>
2021-01-28 17:43:30 -0500

    Add note about clearing cache

--- src/init.ts
+++ src/init.ts
@@ -5,6 +5,7 @@ const CACHE_KEYS = ['/courses', '/sections', '/instructors'];
 
 export class InitHandler implements OnModuleInit {
 	async onModuleInit() {
+		// Clear cache upon startup to allow for schema changes
 		await Promise.all(CACHE_KEYS.map(async k => deleteByKey(k)));
 	}
 }
