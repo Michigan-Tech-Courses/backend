5fc4db7f5ac777dc1073e089e2573a194e2a844b
Max Isom <hi@maxisom.me>
2021-02-04 17:06:37 -0600

    Add tests for PassFailDrop controller

---
+++ src/passfaildrop/passfaildrop.controller.spec.ts
@@ -0,0 +1,69 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {PassFailDrop, Semester} from '@prisma/client';
+import {PrismaService} from 'src/prisma/prisma.service';
+import {PassFailDropController} from './passfaildrop.controller';
+import {PassFailDropModule} from './passfaildrop.module';
+
+describe('PassFailDropController', () => {
+	let controller: PassFailDropController;
+
+	const prismaMock = {
+		passFailDrop: {
+			groupBy: jest.fn()
+		}
+	};
+
+	const record: PassFailDrop = {
+		courseSubject: 'CS',
+		courseCrse: '1000',
+		year: 2020,
+		semester: Semester.FALL,
+		section: '0A',
+		failed: 0,
+		dropped: 0,
+		total: 10
+	};
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [PassFailDropModule]
+		})
+			.overrideProvider(PrismaService)
+			.useValue(prismaMock)
+			.compile();
+
+		controller = module.get<PassFailDropController>(PassFailDropController);
+	});
+
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
+
+	it('should return all records', async () => {
+		prismaMock.passFailDrop.groupBy.mockResolvedValue([{
+			courseSubject: record.courseSubject,
+			courseCrse: record.courseCrse,
+			year: record.year,
+			semester: record.semester,
+			avg: {
+				dropped: record.dropped,
+				failed: record.failed,
+				total: record.total
+			}
+		}]);
+
+		expect(await controller.getAll()).toStrictEqual({
+			[`${record.courseSubject}${record.courseCrse}`]: [{
+				dropped: record.dropped,
+				failed: record.failed,
+				total: record.total,
+				semester: record.semester,
+				year: record.year
+			}]
+		});
+	});
+
+	afterEach(() => {
+		prismaMock.passFailDrop.groupBy.mockClear();
+	});
+});
