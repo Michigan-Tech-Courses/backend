4fc6ff03ff56f3c3571146a2289d6b04e28ceac0
Max Isom <hi@maxisom.me>
2022-10-26 22:48:30 -0700

    TEST: don't manually call .connect()

--- src/pool/pool.service.ts
+++ src/pool/pool.service.ts
@@ -1,25 +1,11 @@
-import type {OnModuleInit, OnModuleDestroy} from '@nestjs/common';
-import {Logger} from '@nestjs/common';
 import {Pool} from 'pg';
 
-export class PoolService extends Pool
-	implements OnModuleInit, OnModuleDestroy {
-	private readonly logger = new Logger(PoolService.name);
+export class PoolService extends Pool {
 	constructor() {
 		// Todo: pull from config
 		super({
 			connectionString: process.env.DATABASE_URL,
 			max: 10
 		});
-	}
-
-	async onModuleInit() {
-		this.logger.log('Connecting to database pool...');
-		await this.connect();
-		this.logger.log('Connected to database pool.');
-	}
-
-	async onModuleDestroy() {
-		await this.end();
 	}
 }
