758be45891335f3ae0fa05f07677fe16ff1b657f
Max Isom <hi@maxisom.me>
2022-10-26 21:47:33 -0700

    Log when connecting to database

--- src/pool/pool.service.ts
+++ src/pool/pool.service.ts
@@ -1,8 +1,10 @@
 import type {OnModuleInit, OnModuleDestroy} from '@nestjs/common';
+import {Logger} from '@nestjs/common';
 import {Pool} from 'pg';
 
 export class PoolService extends Pool
 	implements OnModuleInit, OnModuleDestroy {
+	private readonly logger = new Logger(PoolService.name);
 	constructor() {
 		// Todo: pull from config
 		super({
@@ -12,7 +14,9 @@ export class PoolService extends Pool
 	}
 
 	async onModuleInit() {
+		this.logger.log('Connecting to database pool...');
 		await this.connect();
+		this.logger.log('Connected to database pool.');
 	}
 
 	async onModuleDestroy() {
