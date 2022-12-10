98434baef335102ab0e69f480a44449da1d3756a
Max Isom <hi@maxisom.me>
2022-12-10 13:13:26 -0800

    Fix crontab

--- src/tasks/crontab.ts
+++ src/tasks/crontab.ts
@@ -1,8 +1,8 @@
 export const crontab
 = `
-10 * * * * scrape-instructors ?max=0
-20 */2 * * * scrape-rate-my-professors ?max=0
-40 * * * * scrape-sections ?max=0
-45 * * * * scrape-transfer-courses ?max=0
-50 * * * * scrape-section-details ?max=0
+10 * * * * scrape-instructors ?max=1
+20 */2 * * * scrape-rate-my-professors ?max=1
+40 * * * * scrape-sections ?max=1
+45 * * * * scrape-transfer-courses ?max=1
+50 * * * * scrape-section-details ?max=1
 `;
