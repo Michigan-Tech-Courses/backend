cc186152ca2f0d45d9c3034f8be9946accbae2bf
Max Isom <hi@maxisom.me>
2022-12-10 15:27:43 -0800

    Fix Sentry pg integration?

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,9 +1,12 @@
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
+// eslint-disable-next-line unused-imports/no-unused-imports-ts
+import pg from 'pg';
 import {ProfilingIntegration} from '@sentry/profiling-node';
 
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
+	debug: true,
 	tracesSampleRate: 1,
 	integrations: [
 		new Tracing.Integrations.Postgres(),
