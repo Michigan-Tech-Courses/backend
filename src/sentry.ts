ccc502b8479cc9ae6eb380ebe7074764c02bebfc
Max Isom <hi@maxisom.me>
2022-12-10 12:46:26 -0800

    Add profiling

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,5 +1,6 @@
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
+import {ProfilingIntegration} from '@sentry/profiling-node';
 
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
@@ -7,5 +8,7 @@ Sentry.init({
 	integrations: [
 		new Tracing.Integrations.Postgres(),
 		new Sentry.Integrations.Http({tracing: true}),
-	]
+		new ProfilingIntegration()
+	],
+	profilesSampleRate: 1,
 });
