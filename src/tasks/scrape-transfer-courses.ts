0ed2afd0b42b4ac5d5392e6fe7711f45c0489c4c
Max Isom <hi@maxisom.me>
2022-12-17 21:00:40 -0800

    Actually fix

--- src/tasks/scrape-transfer-courses.ts
+++ src/tasks/scrape-transfer-courses.ts
@@ -1,6 +1,7 @@
 import {Injectable, Logger} from '@nestjs/common';
 import {Task, TaskHandler} from 'nestjs-graphile-worker';
 import * as db from 'zapatos/db';
+import type {ITransferCourse} from '@mtucourses/scraper';
 import {FetcherService} from '~/fetcher/fetcher.service';
 import {PoolService} from '~/pool/pool.service';
 import {updateUpdatedAtForUpsert} from '~/lib/db-utils';
@@ -16,17 +17,31 @@ export class ScrapeTransferCoursesTask {
 	async handler() {
 		const extTransferCourses = await this.fetcher.getAllTransferCourses();
 
-		this.logger.log(`Scraped ${extTransferCourses.length} transfer courses...`);
+		// Filter out duplicates
+		// todo: this should be handled by the scraper
+		const extUniqueTransferCoursesMap = new Map<string, ITransferCourse>();
+
+		for (const course of extTransferCourses) {
+			const key = `${course.from.college}-${course.from.crse}-${course.from.subject}-${course.to.crse}-${course.to.subject}-${course.to.credits}`;
+
+			if (!extUniqueTransferCoursesMap.has(key)) {
+				extUniqueTransferCoursesMap.set(key, course);
+			}
+		}
+
+		const extUniqueTransferCourses = Array.from(extUniqueTransferCoursesMap.values());
+
+		this.logger.log(`Scraped ${extTransferCourses.length} transfer courses (${extUniqueTransferCourses.length} unique ones)...`);
 
 		await db.serializable(this.pool, async trx => {
 			await db.sql`ALTER TABLE ${'TransferCourse'} ADD was_seen boolean DEFAULT false`.run(trx);
 
 			// Batch updates
-			for (let i = 0; i <= extTransferCourses.length; i += 100) {
+			for (let i = 0; i <= extUniqueTransferCourses.length; i += 100) {
 				// eslint-disable-next-line no-await-in-loop
 				await db.upsert(
 					'TransferCourse',
-					extTransferCourses.slice(i, i + 100).map(course => ({
+					extUniqueTransferCourses.slice(i, i + 100).map(course => ({
 						fromCollege: course.from.college,
 						fromCollegeState: course.from.state,
 						fromCRSE: course.from.crse,
