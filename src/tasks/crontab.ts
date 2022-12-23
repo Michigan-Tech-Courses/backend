01dc91706ce9f918327a97aaef9368a701c2d236
Max Isom <hi@maxisom.me>
2022-12-23 12:57:10 -0600

    Change max retries to 2

--- src/tasks/crontab.ts
+++ src/tasks/crontab.ts
@@ -1,8 +1,8 @@
 export const crontab
 = `
-10 * * * * scrape-instructors ?max=1
-20 */2 * * * scrape-rate-my-professors ?max=1
-40 * * * * scrape-sections ?max=1
-45 * * * * scrape-transfer-courses ?max=1
-50 * * * * scrape-section-details ?max=1
+10 * * * * scrape-instructors ?max=2
+20 */2 * * * scrape-rate-my-professors ?max=2
+40 * * * * scrape-sections ?max=2
+45 * * * * scrape-transfer-courses ?max=2
+50 * * * * scrape-section-details ?max=2
 `;
