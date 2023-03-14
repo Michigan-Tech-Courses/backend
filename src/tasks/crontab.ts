74628a2fbd68a6be5ec48d0a082731516ded0746
Max Isom <hi@maxisom.me>
2023-03-13 21:38:55 -0700

    Reduce RMP sraping frequency

--- src/tasks/crontab.ts
+++ src/tasks/crontab.ts
@@ -1,7 +1,7 @@
 export const crontab
 = `
 10 * * * * scrape-instructors ?max=2
-20 */2 * * * scrape-rate-my-professors ?max=2
+20 23 * * * scrape-rate-my-professors ?max=2
 40 * * * * scrape-sections ?max=2
 45 * * * * scrape-transfer-courses ?max=2
 50 * * * * scrape-section-details ?max=2
